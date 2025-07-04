272d5ce6cca20e93dadebcf7101745d3d90e6c08
rayhantmt <rayhantmt@gmail.com>
2025-07-05 05:35:36 +0600

    final change on forgot password view

--- lib/app/modules/forgot_password/forgot_password_view.dart
+++ lib/app/modules/forgot_password/forgot_password_view.dart
@@ -10,38 +10,43 @@ class ForgotPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-      title: Text(''),
-    ),
-    body: Padding(
-      padding: const EdgeInsets.only(left: 20,right: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Forgot Password',
-          style: TextStyle(
-      fontWeight: FontWeight.w700,
-      fontSize: 24,
-      color: Color(0xff232F30)
-      
-          ),
-          ),
-          SizedBox(height: Get.height * 0.02),
+      appBar: AppBar(title: Text('')),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Forgot Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                  color: Color(0xff232F30),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
           
-          Text('We’ll send a verification code to your mail ',
-style: TextStyle(
-  fontWeight: FontWeight.w400,
-  fontSize: 16,
-  color: Color(0xff75838D)
-),
-
-          ),SizedBox(height: Get.height * 0.02),
-          Formfield(tittle: 'rayhantmt@gmail.com', obsecuretext: false, keyboardtype: TextInputType.emailAddress),
-          SizedBox(height: Get.height * 0.02),
-          CommonButton(tittle: 'Send Verification Code')
-        ],
+              Text(
+                'We’ll send a verification code to your mail ',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Color(0xff75838D),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Formfield(
+                tittle: 'rayhantmt@gmail.com',
+                obsecuretext: false,
+                keyboardtype: TextInputType.emailAddress,
+              ),
+              SizedBox(height: Get.height * 0.02),
+              CommonButton(tittle: 'Send Verification Code'),
+            ],
+          ),
+        ),
       ),
-    )
-    ,);
+    );
   }
 }
