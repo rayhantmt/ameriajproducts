6c4225e602aa5e0983cff9288719a3c6ad5ef7f3
rayhantmt <rayhantmt@gmail.com>
2025-07-05 05:28:27 +0600

    working on forgot password view

--- lib/app/modules/forgot_password/forgot_password_view.dart
+++ lib/app/modules/forgot_password/forgot_password_view.dart
@@ -5,8 +5,26 @@ class ForgotPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('sex'),
-    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+      title: Text(''),
+    ),
+    body: Padding(
+      padding: const EdgeInsets.only(left: 20,right: 20),
+      child: Column(
+        children: [
+          Text('Forgot Password',
+          style: TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+      color: Color(0xff232F30)
+      
+          ),
+          )
+        ],
+      ),
+    )
+    ,);
   }
 }
