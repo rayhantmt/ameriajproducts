8f8908ef6fae7121144abb403de99c2dc5dbf858
rayhantmt <rayhantmt@gmail.com>
2025-07-05 05:34:14 +0600

    done with forgot password view

--- lib/app/modules/forgot_password/forgot_password_view.dart
+++ lib/app/modules/forgot_password/forgot_password_view.dart
@@ -1,4 +1,7 @@
+import 'package:ameriajproducts/app/common_widgets/common_button.dart';
+import 'package:ameriajproducts/app/common_widgets/formfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
@@ -13,6 +16,7 @@ class ForgotPasswordView extends StatelessWidget {
     body: Padding(
       padding: const EdgeInsets.only(left: 20,right: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Forgot Password',
           style: TextStyle(
@@ -21,7 +25,20 @@ class ForgotPasswordView extends StatelessWidget {
       color: Color(0xff232F30)
       
           ),
-          )
+          ),
+          SizedBox(height: Get.height * 0.02),
+          
+          Text('We’ll send a verification code to your mail ',
+style: TextStyle(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+  color: Color(0xff75838D)
+),
+
+          ),SizedBox(height: Get.height * 0.02),
+          Formfield(tittle: 'rayhantmt@gmail.com', obsecuretext: false, keyboardtype: TextInputType.emailAddress),
+          SizedBox(height: Get.height * 0.02),
+          CommonButton(tittle: 'Send Verification Code')
         ],
       ),
     )
