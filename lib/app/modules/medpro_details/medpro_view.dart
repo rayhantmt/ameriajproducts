dfc6f04cc94a38cff1356ca13eae73c82fc59dd7
rayhantmt <rayhantmt@gmail.com>
2025-07-11 01:00:20 +0600

    working on medpro details screen

--- lib/app/modules/medpro_details/medpro_view.dart
+++ lib/app/modules/medpro_details/medpro_view.dart
@@ -6,7 +6,38 @@ class MedproView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Medpro Status',
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 18,
+          color: Color(0xff131123)
+        ),),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+        Container(
+          height: 28,
+          width: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+        width: 1,
+        color: Color(0xff08692C)
+            ),
+            borderRadius: BorderRadius.circular(4),
+          ),
+        )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
