aea15fd9a698b9061ee09a9b5a7a5a566af39883
rayhantmt <rayhantmt@gmail.com>
2025-07-11 03:13:02 +0600

    medpro history button

--- lib/app/modules/medpro_details/medpro_view.dart
+++ lib/app/modules/medpro_details/medpro_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MedproView extends StatelessWidget {
   const MedproView({super.key});
@@ -7,34 +8,50 @@ class MedproView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Medpro Status',
-        style: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 18,
-          color: Color(0xff131123)
-        ),),
+        title: Text(
+          'Medpro Status',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+            color: Color(0xff131123),
+          ),
+        ),
       ),
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.only(left: 20,right: 20),
+        padding: const EdgeInsets.only(left: 20, right: 20),
         child: Column(
+          
           children: [
+            SizedBox(height: Get.height*0.008,),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-        Container(
-          height: 28,
-          width: 100,
-          decoration: BoxDecoration(
-            border: Border.all(
-        width: 1,
-        color: Color(0xff08692C)
-            ),
-            borderRadius: BorderRadius.circular(4),
-          ),
-        )
+                Container(
+                  height: 28,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Color(0xff08692C)),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.history,
+                      color:Color(0xff08692C),
+                      ),
+                      Text('View History',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Color(0xff08692C)
+                      ),
+                      )
+                    ],
+                  ),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
