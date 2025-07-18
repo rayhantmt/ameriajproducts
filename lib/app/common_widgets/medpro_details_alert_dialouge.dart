0bac9eb35967fe205f57cf0e8f9dc19e9ee26f10
rayhantmt <rayhantmt@gmail.com>
2025-07-19 05:44:12 +0600

    almost done the medpro details

--- lib/app/common_widgets/medpro_details_alert_dialouge.dart
+++ lib/app/common_widgets/medpro_details_alert_dialouge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MedproDetailsAlertDialouge extends StatelessWidget {
   const MedproDetailsAlertDialouge({super.key});
@@ -6,10 +7,11 @@ class MedproDetailsAlertDialouge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
+    backgroundColor: Colors.white,
       child: Padding(
         padding: const EdgeInsets.only(left: 20,right: 20),
         child: Column(
-crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Fill Form',
             style: TextStyle(
@@ -18,6 +20,91 @@ crossAxisAlignment: CrossAxisAlignment.start,
               color: Colors.black
             ),
             ),
+            SizedBox(height: 10,),
+            Text('Name',
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 14,
+              color: Color(0xff4B5563)
+            ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+              
+                border: Border.all(
+                  width: 1,
+                  color: Color(0xff4B5563)
+                )
+              ),
+              child:TextFormField(
+                
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Enter'
+                  
+                ),
+              ),
+              
+            ),
+            SizedBox(height: 20,),
+            Text('Date',
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 14,
+              color: Color(0xff4B5563)
+            ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  width: 1,
+                  color: Color(0xff4B5563)
+                )
+              ),
+              child: TextFormField(
+                
+                decoration: InputDecoration(
+                 hint: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  Text('Select'),
+                  Icon(Icons.calendar_month_outlined)
+                 ],),
+
+                  border: InputBorder.none
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 35,
+                  width: Get.width*0.30,
+                  decoration: BoxDecoration(
+                   color: Color(0xffF3F4F6)
+                  ),
+                  child: Center(child: Text("Cancel")),
+                ),
+                Container(
+                  height: 35,
+                  width: Get.width*0.30,
+                  decoration: BoxDecoration(
+                   color: Color(0xff08692C)
+                  ),
+                  child: Center(child: Text("Save")),
+                )
+              ],
+            )
           ],
         ),
       ),
--- lib/app/modules/medpro_details/medpro_view.dart
+++ lib/app/modules/medpro_details/medpro_view.dart
@@ -24,101 +24,103 @@ class MedproView extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          children: [
-            SizedBox(height: Get.height * 0.008),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: () => Get.toNamed(Approutes.medprohistory),
-                  child: Container(
-                    height: 28,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Color(0xff08692C)),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(Icons.history, color: Color(0xff08692C)),
-                        Text(
-                          'View History',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 12,
-                            color: Color(0xff08692C),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            children: [
+              SizedBox(height: Get.height * 0.008),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.toNamed(Approutes.medprohistory),
+                    child: Container(
+                      height: 28,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Color(0xff08692C)),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.history, color: Color(0xff08692C)),
+                          Text(
+                            'View History',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 12,
+                              color: Color(0xff08692C),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () => showDialog(
-                    context: context,
-                    builder: (context) => MedproDetailsAlertDialouge()
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () => showDialog(
+                      context: context,
+                      builder: (context) => MedproDetailsAlertDialouge()
+                    ),
+                    child: MedproCard(
+                      tittle: 'Vision',
+                      color: Color(0xff4279F5),
+                      icon: Icons.remove_red_eye_outlined,
+                    ),
                   ),
-                  child: MedproCard(
-                    tittle: 'Vision',
-                    color: Color(0xff4279F5),
-                    icon: Icons.remove_red_eye_outlined,
+                  MedproCard(
+                    tittle: 'Immunization',
+                    color: Color(0xffEF4444),
+                    icon: FontAwesomeIcons.syringe,
                   ),
-                ),
-                MedproCard(
-                  tittle: 'Immunization',
-                  color: Color(0xffEF4444),
-                  icon: FontAwesomeIcons.syringe,
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MedproCard(
-                  tittle: 'Hearing',
-                  color: Color(0xff6642F5),
-                  icon: Icons.hearing,
-                ),
-                MedproCard(
-                  tittle: 'Dental',
-                  color: Color(0xff22C55E),
-                  icon: FontAwesomeIcons.tooth,
-                ),
-              ],
-            ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MedproCard(tittle: 'DLC', color: Color(0xffE58B2F), icon: Icons.cookie),
-                MedproCard(tittle: 'HIV', color: Color(0xffEF44B9), icon: FontAwesomeIcons.ribbon)
-              ],
-            ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MedproCard(tittle: 'PHA', color: Color(0xff6642F5), icon: Icons.water_drop_outlined),
-                MedproCard(tittle: 'DNA', color: Color(0xff22C5A4), icon: FontAwesomeIcons.dna)
-              ],
-            ),
-            Expanded(child: SizedBox()),
-            GestureDetector(
-              onTap: () => Get.toNamed(Approutes.mainscreen),
-              child: CommonButton(tittle: 'Save')),
-            SizedBox(height: 10,)
-          ],
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MedproCard(
+                    tittle: 'Hearing',
+                    color: Color(0xff6642F5),
+                    icon: Icons.hearing,
+                  ),
+                  MedproCard(
+                    tittle: 'Dental',
+                    color: Color(0xff22C55E),
+                    icon: FontAwesomeIcons.tooth,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MedproCard(tittle: 'DLC', color: Color(0xffE58B2F), icon: Icons.cookie),
+                  MedproCard(tittle: 'HIV', color: Color(0xffEF44B9), icon: FontAwesomeIcons.ribbon)
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MedproCard(tittle: 'PHA', color: Color(0xff6642F5), icon: Icons.water_drop_outlined),
+                  MedproCard(tittle: 'DNA', color: Color(0xff22C5A4), icon: FontAwesomeIcons.dna)
+                ],
+              ),
+             SizedBox(height: 20,),
+              GestureDetector(
+                onTap: () => Get.toNamed(Approutes.mainscreen),
+                child: CommonButton(tittle: 'Save')),
+              SizedBox(height: 10,)
+            ],
+          ),
         ),
       ),
     );
