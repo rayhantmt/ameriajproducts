d2d43688e3aba1276b31f1eaddf1f6349187c9e9
rayhantmt <rayhantmt@gmail.com>
2025-07-21 23:34:57 +0600

    adding the text widgets on the alert dialouge

--- lib/app/common_widgets/medpro_history_details_alert_dialouge.dart
+++ lib/app/common_widgets/medpro_history_details_alert_dialouge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MedproHistoryDetailsAlertDialouge extends StatelessWidget {
   const MedproHistoryDetailsAlertDialouge({super.key});
@@ -8,17 +9,53 @@ class MedproHistoryDetailsAlertDialouge extends StatelessWidget {
     return Dialog(
       child: Padding(
         padding: const EdgeInsets.only(left: 20,right: 20,top: 20,bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Fill Form',
+        child: SizedBox(
+          height: Get.height*0.4,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Fill Form',
+              style: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 18,
+          color: Colors.black
+              ),
+              ),
+              SizedBox(height: 20,),
+              Text('Name',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 14,
+                color: Color(0xff4B5563)
+              ),
+              ),
+              SizedBox(height: 10,),
+              Text('Enter',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: Color(0xffADAEBC)
+              ),
+              ),
+              SizedBox(height: 20,),
+              Text('Date',
             style: TextStyle(
-        fontWeight: FontWeight.w700,
-        fontSize: 18,
-        color: Colors.black
+              fontWeight: FontWeight.w700,
+              fontSize: 14,
+              color: Color(0xff4B5563)
             ),
-            )
-          ],
+              ),
+               SizedBox(height: 10,),
+              Text('Enter',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: Color(0xffADAEBC)
+              ),
+              ),
+            ],
+
+          ),
         ),
       )
     );
