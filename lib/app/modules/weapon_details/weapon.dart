f29e8df4032b394b69e7941ef0734d6108c90d65
rayhantmt <rayhantmt@gmail.com>
2025-07-24 05:34:31 +0600

    done with date container

--- lib/app/modules/weapon_details/weapon.dart
+++ lib/app/modules/weapon_details/weapon.dart
@@ -7,59 +7,99 @@ class Weapon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-     appBar: AppBar(
-      backgroundColor: Colors.white,
-      title: Text('Weapon Qualification',
-      style: TextStyle(
-        fontWeight: FontWeight.w500,
-        fontSize: 18,
-        color: Color(0xff131123)
-      ),
-      ),
-     ),
-    body: Padding(
-      padding: const EdgeInsets.only(left: 20,
-      
-    right: 20
-      ),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  height: 30,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      
-                      width: 1,
-                      color: Color(0xff08692C)
-                    ),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.history,
-                      color: Color(0xff08692C),
-                      ),
-                      Text('View History',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 12,
-                        color: Color(0xff08692C)
-                      ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Weapon Qualification',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            color: Color(0xff131123),
+          ),
         ),
       ),
-    ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    height: 30,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Color(0xff08692C)),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.history, color: Color(0xff08692C)),
+                        Text(
+                          'View History',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            color: Color(0xff08692C),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Icon(Icons.calendar_month_outlined,
+                  color: Color(0xff1893F8),
+                  ),
+                  Text('Date',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                    color: Color(0xff4B5563)
+                  ),
+                  ),
+                 
+                 
+                ],
+              ),
+              SizedBox(height: 10,),
+               Container(
+                margin: EdgeInsets.only(left: 10,right: 10),
+                    height: 45,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Color(0xffE5E7EB)
+                      ),
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hint: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('23-06-2015',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              color: Color(0xff6B7280)
+                            ),
+                            ),
+                            Icon(Icons.calendar_month_outlined)
+                          ],
+                        )
+                      ),
+                    ),
+                  )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
