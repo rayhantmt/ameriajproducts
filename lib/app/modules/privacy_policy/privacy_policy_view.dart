2ada97787278ed5c1481b3c7906bac7989484f09
rayhantmt <rayhantmt@gmail.com>
2025-07-24 05:15:48 +0600

    everything done of profile section

--- lib/app/modules/privacy_policy/privacy_policy_view.dart
+++ lib/app/modules/privacy_policy/privacy_policy_view.dart
@@ -6,8 +6,32 @@ class PrivacyPolicyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
 appBar: AppBar(
+  backgroundColor: Colors.white,
   title: Text('Privacy Policy'),
+),
+body: Padding(
+  padding: const EdgeInsets.only(left: 20,right: 20),
+  child: SingleChildScrollView(
+    child: Column(
+      children: [
+        Text('Lorem ipsum dolor sit amet consectetur. Ultrices id feugiat venenatis habitant mattis viverra elementum purus volutpat. Lacus eu molestie pulvinar rhoncus integer proin elementum. Pretium sit fringilla massa tristique aenean commodo leo. Aliquet viverra amet sit porta elementum et pellentesque posuere. Ullamcorper viverra tortor lobortis viverra auctor egestas. Nulla condimentum ac metus quam turpis gravida ut velit. Porta justo lacus consequat sed platea. Ut dui massa quam elit faucibus consectetur sapien aenean auctor. Felis ipsum amet justo in. Netus amet in egestas sed auctor lorem. Justo ullamcorper velit habitasse lorem eu arcu. Non enim a elit urna eget nibh quisque donec condimentum. Elit ut pellentesque neque in quis at viverra. Nisl etiam tristique odio eget convallis.',
+        style: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 12,
+          color: Color(0xff838383)
+        ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text('Lorem ipsum dolor sit amet consectetur. Ultrices id feugiat venenatis habitant mattis viverra elementum purus volutpat. Lacus eu molestie pulvinar rhoncus integer proin elementum. Pretium sit fringilla massa tristique aenean commodo leo. Aliquet viverra amet sit porta elementum et pellentesque posuere. Ullamcorper viverra tortor lobortis viverra auctor egestas. Nulla condimentum ac metus quam turpis gravida ut velit. Porta justo lacus consequat sed platea. Ut dui massa quam elit faucibus consectetur sapien aenean auctor. Felis ipsum amet justo in. Netus amet in egestas sed auctor lorem. Justo ullamcorper velit habitasse lorem eu arcu. Non enim a elit urna eget nibh quisque donec condimentum. Elit ut pellentesque neque in quis at viverra. Nisl etiam tristique odio eget convallis.',
+        style: TextStyle(fontWeight: FontWeight.w400,fontSize: 12,color: Color(0xff838383)),
+        )
+      ],
+    ),
+  ),
 ),
     );
   }
