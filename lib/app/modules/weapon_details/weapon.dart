fdd3a7ac05431385558a6a8b2ac2e92b59be192a
rayhantmt <rayhantmt@gmail.com>
2025-07-24 05:25:49 +0600

    working on weapin screen

--- lib/app/modules/weapon_details/weapon.dart
+++ lib/app/modules/weapon_details/weapon.dart
@@ -6,8 +6,60 @@ class Weapon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Center(child: Text('weapon'),),
+      backgroundColor: Colors.white,
+     appBar: AppBar(
+      backgroundColor: Colors.white,
+      title: Text('Weapon Qualification',
+      style: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 18,
+        color: Color(0xff131123)
+      ),
+      ),
+     ),
+    body: Padding(
+      padding: const EdgeInsets.only(left: 20,
+      
+    right: 20
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 30,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      
+                      width: 1,
+                      color: Color(0xff08692C)
+                    ),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.history,
+                      color: Color(0xff08692C),
+                      ),
+                      Text('View History',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Color(0xff08692C)
+                      ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ),
     );
   }
 }
