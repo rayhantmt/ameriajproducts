19b5366191155cd6bc2139b62343d8f26b614355
rayhantmt <rayhantmt@gmail.com>
2025-07-26 00:08:13 +0600

    designing the container

--- lib/app/common_widgets/qualification_container.dart
+++ lib/app/common_widgets/qualification_container.dart
@@ -7,7 +7,14 @@ class QualificationContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        
+        color: Colors.white,
+          boxShadow: [
+          BoxShadow(
+            color: Color(0xff2C78DC).withOpacity(.08),
+            offset: const Offset(0, 3),
+            blurRadius: 12,
+          ),
+        ],
       ),
     );
   }
