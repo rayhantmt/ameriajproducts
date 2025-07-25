4f78f2278798086bc09bdf5565be7facfc87e215
rayhantmt <rayhantmt@gmail.com>
2025-07-26 01:11:27 +0600

    whole weapon design done

--- lib/app/modules/weapon_details/weapon.dart
+++ lib/app/modules/weapon_details/weapon.dart
@@ -224,6 +224,46 @@ class Weapon extends StatelessWidget {
                   SizedBox(height: 10,),
                   QualificationContainer(title: 'Expert', img: Appimages.expert)
                 ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: 45,
+                    width: Get.width*0.4,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color(0xffF3F4F6)
+                    ),
+                    child: Center(
+                      child: Text('Cancel',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                        color: Color(0xff727272)
+                      ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    width: Get.width*0.4,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color(0xff08692C)
+                    ),
+                    child: Center(
+                      child: Text('Submit',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                        color: Color(0xffFDFDFD)
+                      ),
+                      ),
+                    ),
+                  )
+                ],
               )
             ],
           ),
