e5ad284b4addf3f7250cae912958c4129ba14a19
rayhantmt <rayhantmt@gmail.com>
2025-07-26 01:59:24 +0600

    half done of history

--- lib/app/modules/weapon_history_details/weapon_history_details.dart
+++ lib/app/modules/weapon_history_details/weapon_history_details.dart
@@ -8,12 +8,79 @@ class WeaponHistoryDetails extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text('Weapon Qualification - June 21, 2025',
         style: TextStyle(
           fontWeight: FontWeight.w500,
           fontSize: 18,
           color: Color(0xff131123)
         ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20,top: 30),
+        child: Column(
+          children: [
+            Container(
+              height: 170,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                 boxShadow: [
+            BoxShadow(
+              color: Color(0xff2C78DC).withOpacity(.08),
+              offset: const Offset(0, 3),
+              blurRadius: 12,
+            ),
+          ],
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.calendar_month,
+                      color: Color(0xff1893F8),
+                      ),
+                      Text('Date :',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14,
+                        color: Color(0xff4B5563)
+                      ),
+                      ),
+                      Text('23-06-2015',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                        color: Color(0xff6B7280)
+                      ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.group,
+                      color: Color(0xff22C55E),
+                      ),
+                      Text('Name :',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14,
+                        color: Color(0xff4B5563)
+                      ),
+                      ),
+                      Text('Weapon Name',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                        color: Color(0xff6B7280)
+                      ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
