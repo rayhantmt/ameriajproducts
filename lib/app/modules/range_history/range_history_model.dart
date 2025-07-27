ec9a57cb7d3cd5e21faa17ff83ee0c4f74fff21f
rayhantmt <rayhantmt@gmail.com>
2025-07-27 16:51:23 +0600

    range history done

---
+++ lib/app/modules/range_history/range_history_model.dart
@@ -0,0 +1,19 @@
+class RangeHistoryModel {
+  final String tittle;
+  final String date;
+
+  RangeHistoryModel({required this.tittle, required this.date});
+  
+}
+class rangehistorydata{
+  List<RangeHistoryModel> rangehistory=[
+
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+    RangeHistoryModel(tittle: 'Range Qualification', date: 'Oct 19, 2024'),
+  ];
+}
--- lib/app/modules/range_history/range_history_view.dart
+++ lib/app/modules/range_history/range_history_view.dart
@@ -1,12 +1,75 @@
+
+import 'package:ameriajproducts/app/modules/range_history/range_history_model.dart';
+import 'package:ameriajproducts/app/modules/weapon_history/weapon_history_model.dart';
+import 'package:ameriajproducts/app/routes/app_routes.dart';
+import 'package:ameriajproducts/app/utils/appimages.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class RangeHistoryView extends StatelessWidget {
   const RangeHistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
+    final data = rangehistorydata().rangehistory;
 
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Range Qualification History',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            color: Color(0xff131123),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: Container(
+            height: 70,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff2C78DC).withOpacity(.08),
+                  offset: const Offset(0, 3),
+                  blurRadius: 12,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                SvgPicture.asset(Appimages.range),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [Text(data[index].tittle), Text(data[index].date)],
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () => Get.toNamed(Approutes.weaponhistorydetails),
+                  child: Text(
+                    'View Details',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                      color: Color(0xff1893F8),
+                    ),
+                  ),
+                ),
+                Icon(Icons.arrow_forward, color: Color(0xff1893F8)),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
