19b4ce86cbee38929c066b121124a04a40a110fa
rayhantmt <rayhantmt@gmail.com>
2025-08-17 02:21:30 +0600

    changed the scafold background color of subscrioption

--- lib/app/modules/subscription/subscription_view.dart
+++ lib/app/modules/subscription/subscription_view.dart
@@ -7,7 +7,9 @@ class SubscriptionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text('Subscription'),
       ),
       body: Padding(
