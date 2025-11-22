d7cbcbb7927ee84d5b08e835b5b672543233d60d
rayhantmt <rayhantmt@gmail.com>
2025-11-22 23:41:01 +0600

    implemented the controller in the binding class

--- lib/app/modules/range_history_details/range_history_details_binding.dart
+++ lib/app/modules/range_history_details/range_history_details_binding.dart
@@ -1,8 +1,9 @@
-import 'package:get/get_instance/get_instance.dart';
+import 'package:ameriajproducts/app/modules/range_history_details/range_history_details_controller.dart';
+import 'package:get/get.dart';
 
 class RangeHistoryDetailsBinding extends Bindings{
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.lazyPut<RangeHistoryDetailsController>(() => RangeHistoryDetailsController(),);
   }
 }
