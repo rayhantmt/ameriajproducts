b815ae4c98c6470209606b8d4823a4b7763233fd
rayhantmt <rayhantmt@gmail.com>
2025-11-22 23:55:22 +0600

    fixed a render issue

--- lib/app/modules/range_detailss/range_details_view.dart
+++ lib/app/modules/range_detailss/range_details_view.dart
@@ -37,7 +37,7 @@ class RangeDetailsView extends StatelessWidget {
                     onTap: () => controller.fetchRangeQualifications(),
                     child: Container(
                       height: 30,
-                      width: 100,
+                      width: 110,
                       decoration: BoxDecoration(
                         border: Border.all(width: 1, color: Color(0xff08692C)),
                         borderRadius: BorderRadius.circular(4),
