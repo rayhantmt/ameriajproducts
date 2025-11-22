98cf879a6a914fd363eb00fca0dbec9d5c0b34af
rayhantmt <rayhantmt@gmail.com>
2025-11-23 04:50:37 +0600

    passig weapon data

--- lib/app/modules/weapon_history/weapon_history_view.dart
+++ lib/app/modules/weapon_history/weapon_history_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:ameriajproducts/app/modules/weapon_history/weapon_history_controller.dart';
 import 'package:ameriajproducts/app/routes/app_routes.dart';
 import 'package:ameriajproducts/app/utils/appimages.dart';
@@ -12,7 +11,7 @@ class WeaponHistoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-final controller=Get.find<WeaponHistoryController>();
+    final controller = Get.find<WeaponHistoryController>();
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -28,58 +27,67 @@ final controller=Get.find<WeaponHistoryController>();
         ),
       ),
       body: Obx(() {
-  if (controller.weaponQualifications.isEmpty) {
-    return const Center(
-      child: Text("No history found"),
-    );
-  } else {
-    return ListView.builder(
-        itemCount: controller.weaponQualifications.length,
-        itemBuilder: (context, index)
-        
-         => Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-          child: Container(
-            height: 70,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff2C78DC).withOpacity(.08),
-                  offset: const Offset(0, 3),
-                  blurRadius: 12,
-                ),
-              ],
-            ),
-            child: Row(
-              children: [
-                SvgPicture.asset(Appimages.weapon),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [Text(controller.weaponQualifications[index].title), Text(controller.weaponQualifications[index].displayDate)],
-                ),
-                Spacer(),
-                GestureDetector(
-                  onTap: () => Get.toNamed(Approutes.weaponhistorydetails),
-                  child: Text(
-                    'View Details',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 12,
-                      color: Color(0xff1893F8),
+        if (controller.weaponQualifications.isEmpty) {
+          return const Center(child: Text("No history found"));
+        } else {
+          return ListView.builder(
+            itemCount: controller.weaponQualifications.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: Container(
+                height: 70,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xff2C78DC).withOpacity(.08),
+                      offset: const Offset(0, 3),
+                      blurRadius: 12,
                     ),
-                  ),
+                  ],
                 ),
-                Icon(Icons.arrow_forward, color: Color(0xff1893F8)),
-              ],
+                child: Row(
+                  children: [
+                    SvgPicture.asset(Appimages.weapon),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(controller.weaponQualifications[index].title),
+                        Text(
+                          controller.weaponQualifications[index].displayDate,
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () => Get.toNamed(Approutes.weaponhistorydetails,
+                      arguments: {
+                        'date':controller.weaponQualifications[index].displayDate,
+                        'name':controller.weaponQualifications[index].weaponName,
+                        'pass':controller.weaponQualifications[index].pass,
+                        'score':controller.weaponQualifications[index].score,
+                        'qualification':controller.weaponQualifications[index].qualificationLevel
+                      }
+                      
+                      ),
+                      child: Text(
+                        'View Details',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                          color: Color(0xff1893F8),
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.arrow_forward, color: Color(0xff1893F8)),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ),
-      ); 
-  }
-})
-
+          );
+        }
+      }),
     );
   }
 }
