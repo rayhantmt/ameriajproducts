698957ae115ed6c8a0aab9ad7ba504cf7e8e2a2e
rayhantmt <rayhantmt@gmail.com>
2025-11-23 02:15:19 +0600

    added the controller

--- lib/app/modules/range_detailss/range_binding.dart
+++ lib/app/modules/range_detailss/range_binding.dart
@@ -1,12 +1,12 @@
 import 'package:ameriajproducts/app/common_widgets/qualification_controller.dart';
-import 'package:ameriajproducts/app/modules/range_history/range_history_controller.dart';
+import 'package:ameriajproducts/app/modules/range_detailss/range_controller.dart';
 import 'package:get/get.dart';
 
 class RangeBinding extends Bindings{
   @override
   void dependencies() {
     // TODO: implement dependencies
-    Get.lazyPut<RangeHistoryController>(() => RangeHistoryController(),);
+    Get.lazyPut<RangeController>(() => RangeController(),);
     Get.lazyPut<QualificationController>(() => QualificationController(),);
   }
 }
