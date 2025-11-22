19a992dec09c2822009e4585ab0efb8cd927e12c
rayhantmt <rayhantmt@gmail.com>
2025-11-22 23:39:33 +0600

    created the binding class

---
+++ lib/app/modules/range_history_details/range_history_details_binding.dart
@@ -0,0 +1,8 @@
+import 'package:get/get_instance/get_instance.dart';
+
+class RangeHistoryDetailsBinding extends Bindings{
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+  }
+}
