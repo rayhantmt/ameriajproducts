a633bb922d12f5cd675f378423746ab9bae5a09d
rayhantmt <rayhantmt@gmail.com>
2025-11-25 02:19:44 +0600

    added the controller

--- lib/app/modules/fitness_details/fitness_binding.dart
+++ lib/app/modules/fitness_details/fitness_binding.dart
@@ -1,4 +1,5 @@
 import 'package:ameriajproducts/app/common_widgets/qualification_controller.dart';
+import 'package:ameriajproducts/app/modules/fitness_details/fitness_controller.dart';
 import 'package:ameriajproducts/app/modules/weapon_details/weapon_controller.dart';
 import 'package:get/get.dart';
 
@@ -7,5 +8,6 @@ class FitnessBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<WeaponController>(() => WeaponController());
      Get.lazyPut<QualificationController>(() => QualificationController());
+     Get.lazyPut<FitnessController>(() => FitnessController(),);
   }
 }
