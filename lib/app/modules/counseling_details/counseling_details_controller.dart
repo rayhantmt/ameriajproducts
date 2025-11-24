2ea1f0d28780d8ec447eb66c65f15e1e557838b8
rayhantmt <rayhantmt@gmail.com>
2025-11-24 22:59:28 +0600

    added the text controllers

--- lib/app/modules/counseling_details/counseling_details_controller.dart
+++ lib/app/modules/counseling_details/counseling_details_controller.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class CounselingDetailsController extends GetxController{
-  
+  final namecontroller=TextEditingController();
+  final datecontroller=TextEditingController();
+  final nextdatecontroller=TextEditingController();
+  final counseledbycontroller=TextEditingController();
+  final notescontroller=TextEditingController();
 }
