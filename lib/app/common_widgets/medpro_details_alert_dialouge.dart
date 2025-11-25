f0c8f11c2008c0562bfc13898a885b776cc0394e
rayhantmt <rayhantmt@gmail.com>
2025-11-26 01:41:26 +0600

    done changes

--- lib/app/common_widgets/medpro_details_alert_dialouge.dart
+++ lib/app/common_widgets/medpro_details_alert_dialouge.dart
@@ -4,16 +4,18 @@ import 'package:get/get.dart';
 
 class MedproDetailsAlertDialouge extends StatefulWidget {
   final String type;
-   MedproDetailsAlertDialouge({super.key,required this.type});
+  MedproDetailsAlertDialouge({super.key, required this.type});
 
   @override
-  State<MedproDetailsAlertDialouge> createState() => _MedproDetailsAlertDialougeState();
+  State<MedproDetailsAlertDialouge> createState() =>
+      _MedproDetailsAlertDialougeState();
 }
 
-class _MedproDetailsAlertDialougeState extends State<MedproDetailsAlertDialouge> {
- late TextEditingController nameController;
+class _MedproDetailsAlertDialougeState
+    extends State<MedproDetailsAlertDialouge> {
+  late TextEditingController nameController;
   late TextEditingController dateController;
-@override
+  @override
   void initState() {
     super.initState();
     nameController = TextEditingController();
@@ -30,86 +32,82 @@ class _MedproDetailsAlertDialougeState extends State<MedproDetailsAlertDialouge>
   @override
   Widget build(BuildContext context) {
     return Dialog(
-    backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
 
       child: Padding(
-        padding: const EdgeInsets.only(left: 20,right: 20),
+        padding: const EdgeInsets.only(left: 20, right: 20),
         child: SizedBox(
-          height: Get.height*0.4,
+          height: Get.height * 0.4,
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Fill Form',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 18,
-                color: Colors.black
+              Text(
+                'Fill Form',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
               ),
+              SizedBox(height: 10),
+              Text(
+                'Name',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                  color: Color(0xff4B5563),
+                ),
               ),
-              SizedBox(height: 10,),
-              Text('Name',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 14,
-                color: Color(0xff4B5563)
-              ),
-              ),
-              SizedBox(height: 10,),
+              SizedBox(height: 10),
               Container(
                 height: 50,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
-                
-                  border: Border.all(
-                    width: 1,
-                    color: Color(0xff4B5563)
-                  )
+
+                  border: Border.all(width: 1, color: Color(0xff4B5563)),
                 ),
-                child:TextFormField(
+                child: TextFormField(
                   controller: nameController,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: 'Enter'
-                    
+                    hintText: 'Enter',
                   ),
                 ),
-                
               ),
-              SizedBox(height: 20,),
-              Text('Date',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 14,
-                color: Color(0xff4B5563)
+              SizedBox(height: 20),
+              Text(
+                'Date',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                  color: Color(0xff4B5563),
+                ),
               ),
-              ),
-              SizedBox(height: 10,),
+              SizedBox(height: 10),
               Container(
                 height: 50,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
-                  border: Border.all(
-                    width: 1,
-                    color: Color(0xff4B5563)
-                  )
+                  border: Border.all(width: 1, color: Color(0xff4B5563)),
                 ),
                 child: TextFormField(
                   controller: dateController,
                   decoration: InputDecoration(
-                   hint: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                    Text('Select'),
-                    Icon(Icons.calendar_month_outlined)
-                   ],),
-          
-                    border: InputBorder.none
+                    hint: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Select'),
+                        Icon(Icons.calendar_month_outlined),
+                      ],
+                    ),
+
+                    border: InputBorder.none,
                   ),
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -117,42 +115,41 @@ class _MedproDetailsAlertDialougeState extends State<MedproDetailsAlertDialouge>
                     onTap: () => Navigator.pop(context),
                     child: Container(
                       height: 35,
-                      width: Get.width*0.30,
-                      decoration: BoxDecoration(
-                       color: Color(0xffF3F4F6)
-                      ),
+                      width: Get.width * 0.30,
+                      decoration: BoxDecoration(color: Color(0xffF3F4F6)),
                       child: Center(child: Text("Cancel")),
                     ),
                   ),
                   GestureDetector(
                     //onTap: () => Navigator.pop(context),
                     onTap: () {
-  final controller = Get.find<MedproDetailsController>();
-  controller.addItem(
-    widget.type,
-    nameController.text.trim(),
-    dateController.text.trim(),
-  );
-  Navigator.pop(context);
-},
+                      final controller = Get.find<MedproDetailsController>();
+                      controller.addItem(
+                        widget.type,
+                        nameController.text.trim(),
+                        dateController.text.trim(),
+                      );
+                      Navigator.pop(context);
+                    },
 
                     child: Container(
                       height: 35,
-                      width: Get.width*0.30,
-                      decoration: BoxDecoration(
-                       color: Color(0xff08692C)
+                      width: Get.width * 0.30,
+                      decoration: BoxDecoration(color: Color(0xff08692C)),
+                      child: Center(
+                        child: Text(
+                          "Save",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                      child: Center(child: Text("Save",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color: Colors.white
-                      ),
-                      )),
                     ),
-                  )
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
