6d4f7b0617cbbb01e3b7496690ba507944198eb7
rayhantmt <rayhantmt@gmail.com>
2025-11-26 01:45:20 +0600

    added the medpro api

--- lib/app/core/api_config/api_config.dart
+++ lib/app/core/api_config/api_config.dart
@@ -1,7 +1,6 @@
-// core/config/api_config.dart
-
 class ApiConfig {
-  static const String baseUrl = 'https://readiness-track-server.onrender.com/api/v1';
+  static const String baseUrl =
+      'https://readiness-track-server.onrender.com/api/v1';
 
   // Auth Endpoints
   static const String loginEndpoint = '/auth/login';
@@ -10,17 +9,18 @@ class ApiConfig {
   // User Endpoints
   static const String forgetpassword = '/auth/forgot_password';
   static const String otpendpoint = '/auth/verify_otp';
-  static const String restpasswordendpoint='/auth/reset_password';
-  static const String changepasswordendpoint='/auth/update_password';
-  static const String psotappointment='/Appointment';
-  static const String getAppointmentEndpoint='/Appointment';
-  static const String track='/track';
-  static const String trackhistory='/track?trackType=';
-  static const String weaponqualification='/weapon-qualification';
-  static const String getRangeQualificationEndpoint='/range-qualification';
-  static const String counseling='/counseling';
-  static const String physical_fitness='/physical-fitness';
-  static const String admin='/adminUser';
+  static const String restpasswordendpoint = '/auth/reset_password';
+  static const String changepasswordendpoint = '/auth/update_password';
+  static const String psotappointment = '/Appointment';
+  static const String getAppointmentEndpoint = '/Appointment';
+  static const String track = '/track';
+  static const String trackhistory = '/track?trackType=';
+  static const String weaponqualification = '/weapon-qualification';
+  static const String getRangeQualificationEndpoint = '/range-qualification';
+  static const String counseling = '/counseling';
+  static const String physical_fitness = '/physical-fitness';
+  static const String admin = '/adminUser';
+  static const String medpro = '/medpro';
 
   // Other modules...
 }
