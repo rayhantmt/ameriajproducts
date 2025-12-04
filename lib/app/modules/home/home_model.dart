5236e7f58ff96be5efe9029807e8a26d53d33836
rayhantmt <rayhantmt@gmail.com>
2025-12-05 05:47:29 +0600

    added the boolean value as parameter in home model

--- lib/app/modules/home/home_model.dart
+++ lib/app/modules/home/home_model.dart
@@ -1,18 +1,27 @@
+import 'package:ameriajproducts/app/modules/home/home_contorller.dart';
 import 'package:ameriajproducts/app/utils/appimages.dart';
+import 'package:get/get.dart';
 
 class HomeModel {
   final String name;
   final String img;
-  HomeModel({required this.img, required this.name});
+  final bool status;
+  HomeModel({required this.img,required this.status, required this.name});
 }
 
 class HomeData {
-  final List<HomeModel> homedata = [
-    HomeModel(img: Appimages.medpro, name: 'Medpros'),
-    HomeModel(name: 'Weapon Qualification', img: Appimages.weapon),
-    HomeModel(img: Appimages.range, name: "Range Qualification"),
-    HomeModel(name: 'Physical fitness', img: Appimages.fitness),
-    HomeModel(name: 'Counseling', img: Appimages.counseling),
-    HomeModel(name: 'Admin', img: Appimages.admin),
-  ];
+  late final xyz=Get.find<HomeContorller>();
+  late final List<HomeModel> homedata;
+  
+  HomeData() {
+   var xyz = Get.find<HomeContorller>();
+    homedata = [
+      HomeModel(img: Appimages.medpro, name: 'Medpros',status: xyz.medprostatus.value ),
+      HomeModel(name: 'Weapon Qualification', img: Appimages.weapon,status: xyz.weaponstatus.value),
+      HomeModel(img: Appimages.range, name: "Range Qualification",status: xyz.rangestatus.value),
+      HomeModel(name: 'Physical fitness', img: Appimages.fitness,status: xyz.physicalstatus.value),
+      HomeModel(name: 'Counseling', img: Appimages.counseling,status: xyz.counselingstatus.value),
+      HomeModel(name: 'Admin', img: Appimages.admin,status: xyz.adminstatus.value),
+    ];
+  }
 }
