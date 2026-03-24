502b69ca3f95ae85debeb984914d3b0737d2f57b
rayhantmt <rayhantmt@gmail.com>
2026-03-25 00:06:07 +0600

    formatted document

--- lib/app/common_widgets/profile_container.dart
+++ lib/app/common_widgets/profile_container.dart
@@ -4,47 +4,49 @@ import 'package:flutter_svg/svg.dart';
 class ProfileContainer extends StatelessWidget {
   final String tittle;
   final String details;
-
- final String icon;
-  const ProfileContainer({super.key, required this.tittle, required this.details, required this.icon,});
-
+  final String icon;
+  const ProfileContainer({
+    super.key,
+    required this.tittle,
+    required this.details,
+    required this.icon,
+  });
   @override
   Widget build(BuildContext context) {
     return Container(
-    
-                    height: 45,
-                   // width:  Get.width*0.9,
-                    child: Row(
-                      children: [
-                        SvgPicture.asset(icon),
-                        SizedBox(width: 8,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(tittle,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14,
-                              color: Color(0xff063749)
-                            ),
-                            ),
-                            Text(details,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                            style: TextStyle(
-                              
-                              fontWeight: FontWeight.w400,
-                              fontSize: 12,
-                              color: Color(0xff989898)
-                            ),
-                            ),
-
-                          ],
-                        ),
-                        Spacer(),
-                        Icon(Icons.arrow_forward_ios_outlined)
-                      ],
-                    ),
-                  );
+      height: 45,
+      // width:  Get.width*0.9,
+      child: Row(
+        children: [
+          SvgPicture.asset(icon),
+          SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                tittle,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  color: Color(0xff063749),
+                ),
+              ),
+              Text(
+                details,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color(0xff989898),
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Icon(Icons.arrow_forward_ios_outlined),
+        ],
+      ),
+    );
   }
 }
